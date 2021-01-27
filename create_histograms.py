c64022b3f20574b61371c46104fd0873bf2830ae
dey4ss <27929897+dey4ss@users.noreply.github.com>
2021-01-27 15:56:13 +0100

    remove older hist creation

--- create_histograms.py
+++ create_histograms.py
@@ -60,102 +60,173 @@ def parse_args():
     return vars(ap.parse_args())
 
 
-def get_hist(all_changes, daily_changes, min_supp, max_supp, min_conf, days, num_days):
-    hists = defaultdict(dict)
-    actives = dict()
-    already_done = defaultdict(set)
-    print(f"start: {datetime.now()}")
+class Histogram:
+    def __init__(self):
+        self._is_setup = False
+        self._actual_antecedent = 0
+        self._occurence_count = 0
+
+    def is_setup(self):
+        return self._is_setup
+
+    def setup(self, bin_count, occurences_right):
+        if self._is_setup:
+            raise RuntimeError(f"{self.__class__.__name__} has already been set up.")
+        self._bins = [0 for _ in range(bin_count)]
+        self._count_antecedent = occurences_right
+        self._is_setup = True
+
+    def add_occurence(self, bin):
+        if not self._is_setup:
+            raise RuntimeError(f"{self.__class__.__name__} needs to be set up first.")
+        self._bins[bin] += 1
+        self._occurence_count += 1
+
+    def add_antecedent_occurence(self):
+        self._actual_antecedent += 1
+
+    def antecedent_occurences(self):
+        return self._actual_antecedent
+
+    def bins(self):
+        return self._bins
+
+    def confidence(self):
+        return self._occurence_count / self._count_antecedent
+
+    def abs_support(self):
+        return self._occurence_count
+
+
+def get_hist(all_changes, daily_changes, min_supp_abs, max_supp_abs, min_conf, days, num_days, candidates):
+    hists = defaultdict(lambda: defaultdict(Histogram))
+    start = datetime.now()
+    print(f"start : {start}")
     sum_days = len(days)
-    support_threshold = math.ceil(min_supp * sum_days)
+
+    # index of changes within num days
+    # change -> days since last occurence
+    active_changes = dict()
+
+    # index consequent -> antecedents
+    pruned_combinations = defaultdict(set)
 
     for date, day_index in zip(days, range(sum_days)):
         print(date)
         active_today = dict()
         outdated = set()
-        # update actives
+        # gather changes of current day, update antecedent counts
         for change in daily_changes[date]:
             active_today[change] = 0
-        for change in actives:
-            actives[change] += 1
-            if actives[change] >= num_days:
+            for hist in hists[change].values():
+                hist.add_antecedent_occurence()
+
+        # update time since occurence for older antecedents
+        for change in active_changes:
+            active_changes[change] += 1
+            if active_changes[change] >= num_days:
                 outdated.add(change)
-        actives.update(active_today)
+
+        # merge, remove too old antecedents
+        active_changes.update(active_today)
         for change in outdated:
-            del actives[change]
+            del active_changes[change]
+
         # skip if min support cannot be reached
         can_shortcut_support = sum_days - day_index < num_days
 
+        antecedent_candidates = set(active_changes.keys())
+        consequents = candidates & daily_changes[date]
+
         # begin with real work:
-        for change in active_today:
-            for other_change in actives:
-                if other_change == change:
+        for consequent in consequents:
+            antecedents = antecedent_candidates - pruned_combinations[consequent]
+            for antecedent in antecedents:
+                if antecedent == consequent:
                     continue
 
-                occurences_change = all_changes[change]
-                occurences_other = all_changes[other_change]
+                hist = hists[antecedent][consequent]
+                occurences_antecedent = all_changes[antecedent]
+                occurences_consequent = all_changes[consequent]
 
-                # skip if this has been done before:
-                if change in hists[other_change] or change in already_done[other_change]:
+                # check if histogram is already created
+                # prune if min confidence or min support cannot be reached
+                if not hist.is_setup():
+                    maximal_confidence = len(occurences_consequent) / len(occurences_antecedent)
+                    if can_shortcut_support or maximal_confidence < min_conf:
+                        del hists[antecedent][consequent]
+                        pruned_combinations[consequent].add(antecedent)
+                        continue
+                    else:
+                        hist.setup(num_days, len(occurences_antecedent))
+
+                # prune if antecedent has appeared too often to reach min confidence
+                # or too few occurrences are left for reaching min support
+                # or max supp is too high
+                remaining_antecedent_occurences = len(occurences_antecedent) - hist.antecedent_occurences() + 1
+                remaining_consequent_occurences = len(occurences_consequent) - occurences_consequent.index(date)
+                possible_occurences = min(remaining_consequent_occurences, remaining_antecedent_occurences)
+                can_reach_conf = (hist.abs_support() + possible_occurences) / len(occurences_antecedent) >= min_conf
+                can_reach_sup = hist.abs_support() + possible_occurences >= min_supp_abs
+                under_max_sup = hist.abs_support() < max_supp_abs
+
+                if not (can_reach_conf and can_reach_sup and under_max_sup):
+                    del hists[antecedent][consequent]
+                    pruned_combinations[consequent].add(antecedent)
                     continue
 
-                # skip if min confidence cannot be reached
-                confidence_threshold = math.floor(len(occurences_other) * min_conf)
-                if confidence_threshold > len(occurences_change):
-                    already_done[other_change].add(change)
-                    continue
+                # actually add value to histogram
+                hist.add_occurence(active_changes[antecedent])
 
-                if can_shortcut_support:
-                    already_done[other_change].add(change)
-                    continue
+    end = datetime.now()
+    print(f"saving: {end}")
+    print("duration", end - start)
+    del active_changes
+    del all_changes
+    del daily_changes
+    del pruned_combinations
 
-                # create empty histogram
-                hist = [0 for _ in range(num_days)]
+    # remove antecedents without consequents
+    # combinations with low min support / confidence have been removed previously
+    useless_antecedents = set()
+    for antecedent, consequents in hists.items():
+        if len(consequents) == 0:
+            useless_antecedents.add(antecedent)
+            continue
 
-                # move two pointers to occurences of change and other change
-                # to always add other change --> change to histogram
-                ind_change = 0
-                ind_other = 0
-                while ind_other < len(occurences_other) and ind_change < len(occurences_change):
-                    date_other = occurences_other[ind_other]
-                    date_change = occurences_change[ind_change]
-                    while date_change < date_other:
-                        ind_change += 1
-                        if ind_change >= len(occurences_change):
-                            break
-                        date_change = occurences_change[ind_change]
-                    if ind_change >= len(occurences_change):
-                        break
-                    while ind_other < len(occurences_other) - 1 and occurences_other[ind_other + 1] <= date_change:
-                        ind_other += 1
-                        date_other = occurences_other[ind_other]
-                    deviation = days.index(date_change) - days.index(date_other)
-                    if deviation < num_days - 1:
-                        hist[days.index(date_change) - days.index(date_other)] += 1
-                    ind_other += 1
-                    ind_change += 1
-                # only add if satisfies min confidence
-                change_count = sum(hist)
-                if change_count >= max(support_threshold, confidence_threshold):
-                    hists[other_change][change] = hist
-                already_done[other_change].add(change)
+    for antecedent in useless_antecedents:
+        del hists[antecedent]
 
-    print(f"saving: {datetime.now()}")
-    with open("histograms_tables.json", "w") as f:
-        json.dump(hists, f)
+    result = {
+        antecedent: {
+            consequent: [hist.abs_support(), hist.confidence(), hist.bins()] for consequent, hist in consequents.items()
+        }
+        for antecedent, consequents in hists.items()
+    }
+
+    with open("histograms_tables-t.json", "w") as f:
+        json.dump(result, f)
 
     print(f"end: {datetime.now()}")
 
 
 def main():
+    start = datetime.now()
+    print("start program:", start)
     args = parse_args()
-    with open(args["change_file"]) as f:
-        all_changes = json.load(f)
-    daily_changes = defaultdict(set)
+    min_supp = args["min_supp"]
     actual_days = list(
         sorted({file_name[:10] for file_name in os.listdir(args["change_dir"]) if file_name.startswith("20")})
     )
-    min_supp = args["min_supp"]
     support_threshold = math.ceil(min_supp * len(actual_days))
+
+    # get index change -> dates
+    with open(args["change_file"]) as f:
+        all_changes = json.load(f)
+
+    # build index date -> changes
+    # remove change if min support to low
+    daily_changes = defaultdict(set)
     too_infrequent_changes = set()
     for change, occurences in all_changes.items():
         if len(occurences) < support_threshold:
@@ -166,14 +237,17 @@ def main():
     for change in too_infrequent_changes:
         del all_changes[change]
 
+    candidates = set(all_changes.keys())
+
     get_hist(
         all_changes,
         daily_changes,
-        min_supp,
-        args["max_supp"],
+        support_threshold,
+        math.floor(args["max_supp"] * len(actual_days)),
         args["min_conf"],
         actual_days,
         args["num_bins"],
+        candidates,
     )
 
 
